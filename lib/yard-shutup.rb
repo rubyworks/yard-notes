8553854316b8ad9a75f20187e474c6cb4bfc6bb3
7rans <transfire@gmail.com>
2011-06-12 07:10:46 -0400

    handle missing tags in Library instead of Docstring

--- lib/yard-shutup.rb
+++ lib/yard-shutup.rb
@@ -1,39 +1,49 @@
-require 'yard/docstring'
-
+# @TODO Add command option to turn warnings back on.
 module YARD
-  class Docstring
 
-    # Creates a tag from the `Tags::DefaultFactory` tag factory.
-    # 
-    # To add an already created tag object, use `#add_tag`
+  module Tags
+
+    # Alternate factory in which 
     #
-    # @TODO This is my tood note!
-    # @TODO Add command option to turn warnings back on.
-    # @TODO Keep track of auto-defined tags for later listing or template.
-    #
-    # @param [String] tag_name the tag name
-    # @param [String] tag_buf the text attached to the tag with newlines removed.
-    # @return [Tags::Tag, Tags::RefTag] a tag
-    def create_tag(tag_name, tag_buf)
-      if tag_buf =~ /\A\s*(?:(\S+)\s+)?\(\s*see\s+(\S+)\s*\)\s*\Z/
-        return create_ref_tag(tag_name, $1, $2)
+    # @NOTE I have a feeling this might all end up here
+    # once lsegal sees what his code is actually doing.
+    class AutoFactory < DefaultFactory
+    end
+
+    class Library
+
+      class << self
+        # Sets the list of tags that are not explicitly defined by the 
+        # YARD proper, nor by the user on the cammnd line via `--tag`.
+        # These are tags are recorded as "developer's tags" and not
+        # displayed in the general documentation.
+        #
+        # @return [Array<Symbol>] a list of transitive tags
+        # @since 0.6.0
+        attr_accessor :developers_tags
       end
 
-      tag_factory = Tags::Library.instance
-      tag_method = "#{tag_name}_tag"
-      if tag_name
-        if tag_factory.respond_to?(tag_method)
-          add_tag(*[tag_factory.send(tag_method, tag_buf)].flatten)
-        else
+      self.developers_tags = []
+
+      # If tag is missing then auto-define it.
+      #
+      # @TODO This is an example.
+      def respond_to?(tag_method)
+        if md = /_tag$/.match(tag_method.to_s)
+          tag_name = md.pre_match
           Tags::Library.define_tag(tag_name.to_s.capitalize, tag_name)
-          add_tag(*[tag_factory.send(tag_method, tag_buf)].flatten)
+          Tags::Library.developers_tags << tag_name.to_sym
+          true
+        else
+          super(tag_method)
         end
-      else
-        log.warn "Unnamed tag @#{tag_name}" + (object ? " in file `#{object.file}` near line #{object.line}" : "")
       end
-    rescue Tags::TagFormatError
-      log.warn "Invalid tag format for @#{tag_name}" + (object ? " in file `#{object.file}` near line #{object.line}" : "")
+
     end
 
   end
+
 end
+
+YARD::Tags::Library.default_factory = YARD::Tags::AutoFactory.new
+
